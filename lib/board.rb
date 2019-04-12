b5ddd1b1a71357f9a067729231176060ab43e294
Aaron Collins <aa.collins.95@gmail.com>
2019-04-12 16:05:10 -0400

    switched letter and number labels

--- lib/board.rb
+++ lib/board.rb
@@ -16,20 +16,20 @@ class Board
     print "\n" * 15
     (0...@height).reverse_each { |row| draw_row(row)}
     print "   "
-    (1..@width).each{ |x| print "\e[34m#{x.to_s}\e[0m" + "  " }
+    (0...@width).each{ |x| print "\e[34m#{row_to_letter(x)}\e[0m" + "  " }
     print "\n"
   end
 
   def draw_row(y)
     size = 2
     space = " " * size
-    print "\e[34m#{row_to_letter(y)}\e[0m" + space
+    print "\e[34m#{y}\e[0m" + space
     (0...@width).each{ |x| print @squares[[x,y]].to_s + space }
     print "\n" + "\n" * (size-1)
   end
 
   def row_to_letter(row)
-    raises "row needs to be in range" until (0...@height).include?(row)
+    raise "row needs to be in range" until (0...@height).include?(row)
     #gets letter of a particular
     return ("A".ord + row).chr
   end

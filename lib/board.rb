4c63fa5931ad4a2c99dec61193e4105958b189dc
Aaron Collins <aa.collins.95@gmail.com>
2019-04-12 12:50:12 -0400

    board can draw itself with labels

--- lib/board.rb
+++ lib/board.rb
@@ -10,16 +10,24 @@ class Board
     }
   end
 
+
   def draw_board
-    draw_row(0)
+    print "\n" * 15
+    (0...@height).reverse_each { |row| draw_row(row)}
+    (1..@width).each{ |x| print x.to_s + "   " }
+    print "\n"
   end
 
   def draw_row(y)
-    space = " "
+    size = 3
+    space = " " * size
+    (0...@width).each{ |x| print @squares[[x,y]].to_s + space }
+    print row_to_letter(y)
+    print "\n" + "\n" * (size-2)
   end
 
-  def draw_empty
-
+  def row_to_letter(row)
+    return ("A".ord + row).chr
   end
 
 end
--- lib/chess.rb
+++ lib/chess.rb
@@ -5,5 +5,4 @@ require "./lib/unicode"
 
 board = Board.new
 pos = [2,5]
-square = board.squares[pos]
-puts square.to_s
+board.draw_board

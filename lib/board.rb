2a8fe7c46347d48494ab5fc4c9c819de65a2d696
Aaron Collins <aa.collins.95@gmail.com>
2019-04-12 10:37:43 -0400

    squares can have selected bool attribute

--- lib/board.rb
+++ lib/board.rb
@@ -5,10 +5,11 @@ class Board
     @squares = Hash.new
     (0...@height).each { |r|
       (0...@width).each { |c|
-        # print [c,r]
-        # puts " "
         @squares[[c,r]] = Square.new([c,r])
       }
     }
   end
+
+  def draw
+
 end
--- lib/square.rb
+++ lib/square.rb
@@ -2,5 +2,6 @@ class Square
   def initialize(pos)
     @pos = pos
     @full = false
+    @selected = false
   end
 end
